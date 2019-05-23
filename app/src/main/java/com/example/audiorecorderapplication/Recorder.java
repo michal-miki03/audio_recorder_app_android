0c076c4b1ad7dfbb99d0c33bbb3ae69ef59869a0
Michał <m.wiczk@wp.pl>
2019-05-23 22:01:26 +0200

    MainActivity basics adding Recorder and RecordingList activities

---
+++ app/src/main/java/com/example/audiorecorderapplication/Recorder.java
@@ -0,0 +1,13 @@
+package com.example.audiorecorderapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recorder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorder);
+    }
+}
---
+++ app/src/main/java/com/example/audiorecorderapplication/RecordingList.java
@@ -0,0 +1,13 @@
+package com.example.audiorecorderapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording_list);
+    }
+}
